fe88b8b8144607d321ff6fe04fa02e6c6fda465a
Joy Rhee <joyfullyx@gmail.com>
2021-05-24 14:30:47 -0700

    starting seed data

---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const seedUser = require('./user-seeds');
+const seedTrip = require('./trip-seeds');
+const seedActivity = require('./activity-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    await seedUser();
+    console.log('\n----- USERS SEEDED -----\n');
+
+    await seedTrip();
+    console.log('\n----- TRIPS SEEDED -----\n');
+
+    await seedActivity();
+    console.log('\n----- ACTIVITY SEEDED -----\n');
+
+    process.exit(0);
+};
+
+seedAll();
